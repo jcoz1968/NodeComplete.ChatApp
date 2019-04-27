ae2b94f8699fee58f4d1e10f8871f7f497b9715a
Jeffery Cosby <jcoz1968@gmail.com>
2019-04-27 13:48:24 -0500

    form and button states

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,13 +1,24 @@
 const socket = io();
 
+// Elements
+const $messageForm = document.querySelector('#message-form');
+const $messageFormInput = $messageForm.querySelector('input');
+const $messageFormButton = $messageForm.querySelector('button');
+const $locationButton = document.querySelector('#send-location');
+
 socket.on('message', (message) => { 
   console.log(message);
 });
 
-document.querySelector('#message-form').addEventListener('submit', (e) => { 
+$messageForm.addEventListener('submit', (e) => { 
   e.preventDefault();
+  $messageFormButton.setAttribute('disabled', 'disabled');
+
   const message = e.target.elements.message.value;
   socket.emit('sendMessage', message, (error) => { 
+    $messageFormButton.removeAttribute('disabled');
+    $messageFormInput.value = '';
+    $messageFormInput.focus();
     if (error) {
       return console.log(error);
     } 
@@ -19,12 +30,14 @@ document.querySelector('#send-location').addEventListener('click', () => {
   if (!navigator.geolocation) {
     return alert('Geolocation is not supported by your browser.');
   }
+  $locationButton.setAttribute('disabled', 'disabled');
   navigator.geolocation.getCurrentPosition((position) => {
     socket.emit('sendLocation', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     }, () => { 
-        console.log('location shared.')
-    });
+        console.log('location shared.');
+        $locationButton.removeAttribute('disabled');
+      });
   });
 });
