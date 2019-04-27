709832203d00eedf85742a5a0afbeccdd8e08ce1
Jeffery Cosby <jcoz1968@gmail.com>
2019-04-27 14:41:27 -0500

    working with time

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const Filter = require('bad-words');
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+const { generateMessage } = require('./utils/messages');
 
 const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, '../public');
@@ -16,15 +17,16 @@ app.use(express.static(publicDirectoryPath));
 
 io.on('connection', (socket) => {
   console.log('New WebSocket connection');
-  socket.emit('message', 'Welcome');
-  socket.broadcast.emit('message', 'A new user has joined');
+
+  socket.emit('message', generateMessage('Welcome!'));
+  socket.broadcast.emit('message', generateMessage('A new user has joined'));
 
   socket.on('sendMessage', (message, callback) => { 
     const bwfilter = new Filter();
     if (bwfilter.isProfane(message)) {
       return callback('Profanity is not allowed.')
     }
-    io.emit('message', message);
+    io.emit('message', generateMessage(message));
     callback();
   });
 
@@ -34,7 +36,7 @@ io.on('connection', (socket) => {
   });
 
   socket.on('disconnect', () => { 
-    io.emit('message', 'A user has left the chat room');
+    io.emit('message', generateMessage('A user has left the chat room'));
   });
 });
 
