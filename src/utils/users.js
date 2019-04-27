2977534ed9c410395b6c779ff7509ff2b6475372
Jeffery Cosby <jcoz1968@gmail.com>
2019-04-27 15:46:06 -0500

    store users in room 2

--- src/utils/users.js
+++ src/utils/users.js
@@ -1,22 +1,22 @@
-const users = []
+const users = [];
 
 // addUser, removeUser, getUser, getUsersInRoom
 
 const addUser = ({ id, username, room }) => {
     // Clean the data
-    username = username.trim().toLowerCase()
-    room = room.trim().toLowerCase()
+  username = username.trim().toLowerCase();
+  room = room.trim().toLowerCase();
 
     // Validate the data
     if (!username || !room) {
         return {
-            error: 'Username and room are required!'
+          error: 'Username and room are required!'
         }
     }
 
     // Check for existing user
     const existingUser = users.find((user) => {
-        return user.room === room && user.username === username
+      return user.room === room && user.username === username;
     })
 
     // Validate username
@@ -27,28 +27,30 @@ const addUser = ({ id, username, room }) => {
     }
 
     // Store user
-    const user = { id, username, room }
-    users.push(user)
-    return { user }
+  const user = { id, username, room };
+  users.push(user);
+  return { user };
 }
 
 const removeUser = (id) => {
-    const index = users.findIndex((user) => user.id === id)
+  const index = users.findIndex((user) => user.id === id);
 
     if (index !== -1) {
-        return users.splice(index, 1)[0]
+      return users.splice(index, 1)[0];
     }
 }
 
-addUser({
-    id: 22,
-    username: 'Andrew  ',
-    room: '  South Philly'
-})
+const getUser = (id) => { 
+  return users.find((user) => user.id === id);
+}
 
-console.log(users)
+const getUsersInRoom = (room) => { 
+  return users.filter((user) => user.room === room);
+}
 
-const removedUser = removeUser(22)
-
-console.log(removedUser)
-console.log(users)
+module.exports = {
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom
+}
